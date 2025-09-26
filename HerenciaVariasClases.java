fcf1c01899677bb1c910605694c5ea7438ca2420
Zadu <jartetab@unicartagena.edu.co>
2025-09-26 17:11:42 -0500

    Ejercicio 4

---
+++ HerenciaVariasClases.java
@@ -0,0 +1,7 @@
+package Unidad_2_Taller_8_HerenciaSimple;
+
+public class HerenciaVariasClases {
+    //Da error porque no se puede heredar varias clases
+//public class HerenciaMultiplesClases extends Persona Gerente{
+//Da error porque es una propiedad privada Persona.identificacion;
+}
